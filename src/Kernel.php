753ff6ea4b6e5004335eaf23418e79e744210b86
chadda <chadha.essid@insat.ucar.tn>
2024-07-02 14:57:19 +0100

    test de typesense

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use ACSEO\TypesenseBundle\Command\CreateCommand;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
 }
