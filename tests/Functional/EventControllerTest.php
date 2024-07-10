a6befa344064cf422657ebe3cf085557ab1a05e4
chadda <chadha.essid@insat.ucar.tn>
2024-07-10 14:17:06 +0100

    fix: workaround detached object error *clean up objects from the db using a query instead of using doctrine

--- tests/Functional/EventControllerTest.php
+++ tests/Functional/EventControllerTest.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Entity\AdminUser;
+use App\Entity\Event;
 use App\Entity\User;
 use App\Factory\AdminFactory;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Factory\UserFactory;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 
 class EventControllerTest extends WebTestCase
@@ -20,7 +22,7 @@ class EventControllerTest extends WebTestCase
         parent::setUp();
         $this->client = static::createClient();
         $this->userFactory = $this->client->getContainer()->get(UserFactory::class);
-        $this->adminFactory=$this->client->getContainer()->get(AdminFactory::class);
+        $this->adminFactory = $this->client->getContainer()->get(AdminFactory::class);
         $this->entityManager = $this->client->getContainer()->get(EntityManagerInterface::class);
     }
 
@@ -39,33 +41,66 @@ class EventControllerTest extends WebTestCase
         $this->assertRouteSame('app_admin_login');
     }
 
-    public function test_access_to_event_index_for_admins_successful(){
-        $admin=$this->adminFactory->createAdmin('test-admin','password');
+    public function test_access_to_event_index_for_admins_successful()
+    {
+        $admin = $this->adminFactory->createAdmin('test-admin', 'password');
         $this->entityManager->persist($admin);
         $this->entityManager->flush();
         $this->client->loginUser($admin);
-        $this->client->request('GET','/admin/event');
+        $this->client->request('GET', '/admin/event');
         if ($this->client->getResponse()->isRedirect()) {
             $this->client->followRedirect();
         }
-        //in case the server didn't redirect directly
-        $this->client->followRedirect();
         $this->assertRouteSame('app_event_index');
     }
 
+    public function test_show_event_for_admin_successful(): void
+    {
+        $admin = $this->adminFactory->createAdmin('test-admin', 'password');
+        $this->entityManager->persist($admin);
+        $this->entityManager->flush();
+        $this->client->loginUser($admin);
+        $event = new Event();
+        $event->setName('test-event');
+        $this->entityManager->persist($event);
+        $this->entityManager->flush();
+        $crawler = $this->client->request('GET', '/admin/event/' . $event->getId());
+
+        if ($this->client->getResponse()->isRedirect()) {
+            $this->client->followRedirect();
+        }
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_show_event_for_admin_unsuccessful(): void
+    {
+        $admin = $this->adminFactory->createAdmin('test-admin', 'password');
+        $this->entityManager->persist($admin);
+        $this->entityManager->flush();
+        $this->client->loginUser($admin);
+        $event = new Event();
+        $event->setName('test-event');
+        $this->entityManager->persist($event);
+        $this->entityManager->flush();
+        $fakeId = "110";
+        $crawler = $this->client->request('GET', '/admin/event/' . $fakeId);
+        if ($this->client->getResponse()->isRedirect()) {
+            $this->client->followRedirect();
+        }
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+
     protected function tearDown(): void
     {
+        $connection = $this->entityManager->getConnection();
+        $connection->executeQuery('DELETE FROM event WHERE name = "test-event"');
+        $connection->executeQuery('DELETE FROM admin_user WHERE username = "test-admin"');
+        $connection->executeQuery('DELETE FROM user WHERE email = "nonadmin@example.com"');
+
+        $this->entityManager->close();
+        $this->entityManager = null; //
+
         parent::tearDown();
-        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => 'nonadmin@example.com']);
-        $admin=$this->entityManager->getRepository(AdminUser::class)->findOneBy(['username' => 'test-admin']);
-        if ($user) {
-        $this->entityManager->remove($user);
-        $this->entityManager->flush();
-        }
-        if($admin){
-            $this->entityManager->remove($admin);
-            $this->entityManager->flush();
-        }
-        $this->entityManager->clear();
     }
 }
