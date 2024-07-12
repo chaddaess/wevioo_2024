d9b6888bc5fb001b856fbd83252ba4cf95154850
chadda <chadha.essid@insat.ucar.tn>
2024-07-12 13:37:24 +0100

    feature: write integration test:test_toggle_going

--- tests/Integration/HomeControllerIntegrationTest.php
+++ tests/Integration/HomeControllerIntegrationTest.php
@@ -26,20 +26,16 @@ class HomeControllerIntegrationTest extends WebTestCase
         $this->em = $this->client->getContainer()->get(EntityManagerInterface::class);
     }
 
-
-    public function testToggleInterested()
+    public function test_toggle_interested()
     {
-        // Create a user and an event
         $user = $this->userFactory->createUser('test-user@test.com', 'password');
         $event = $this->eventFactory->createEvent();
-
-        // Persist entities
         $entityManager = $this->em;
         $entityManager->persist($user);
         $entityManager->persist($event);
         $entityManager->flush();
-        // Log in user
         $this->client->loginUser($user);
+        $this->client->followRedirects();
 
         // Initial state check
         $this->assertFalse($user->getInterestedIn()->contains($event),'user shouldnt be interested initially');
@@ -47,26 +43,57 @@ class HomeControllerIntegrationTest extends WebTestCase
 
         // Add interest
         $crawler = $this->client->request('GET', '/event/'.$event->getId().'/interested');
-        // Refresh entities to get updated state from the database
         $entityManager->refresh($entityManager->getRepository(User::class)->findOneBy(['email'=>'test-user@test.com']));
         $entityManager->refresh($entityManager->getRepository(Event::class)->findOneBy(['name'=>'test-event']));
         $entityManager->flush();
 
         // Verify user is interested and event interest count has increased
         $this->assertTrue($user->getInterestedIn()->contains($event),'user should be interested');
-        $this->assertGreaterThan(0, $event->getInterested(),'event should now have an interested person');
+        $this->assertSame(1, $event->getInterested(),'event should now have an interested person');
 
 
         // Remove interest
         $crawler = $this->client->request('GET', '/event/'.$event->getId().'/interested');
-        $this->assertResponseRedirects('/event/'.$event->getId());
-        $crawler = $this->client->followRedirect();
         $entityManager->refresh($entityManager->getRepository(User::class)->findOneBy(['email'=>'test-user@test.com']));
         $entityManager->refresh($entityManager->getRepository(Event::class)->findOneBy(['name'=>'test-event']));
         $entityManager->flush();
         // Verify user is no longer interested and event interest count is 0
         $this->assertFalse($user->getInterestedIn()->contains($event),"user shouldnt have events in intersted anymore");
         $this->assertEquals(0, $event->getInterested(),'event shouldnt have any more intersted people ');
+    }
+
+    public function test_toggle_going(){
+        $user = $this->userFactory->createUser('test-user@test.com', 'password');
+        $event = $this->eventFactory->createEvent();
+        $entityManager = $this->em;
+        $entityManager->persist($user);
+        $entityManager->persist($event);
+        $entityManager->flush();
+        $this->client->loginUser($user);
+
+        // Initial state check
+        $this->assertFalse($user->getGoingTo()->contains($event),'user shouldn`t be attending initially');
+        $this->assertSame(0, $event->getAttending(),'event shouldn`t have interested people initially');
+
+        // add `going`
+        $crawler = $this->client->request('GET', '/event/'.$event->getId().'/going');
+        $entityManager->refresh($entityManager->getRepository(User::class)->findOneBy(['email'=>'test-user@test.com']));
+        $entityManager->refresh($entityManager->getRepository(Event::class)->findOneBy(['name'=>'test-event']));
+        $entityManager->flush();
+
+        // Verify user is now going and event attending count has increased
+        $this->assertTrue($user->getGoingTo()->contains($event),'user should be going to event');
+        $this->assertSame(1, $event->getAttending(),'event should now have an attending person');
+
+        // Remove going
+        $crawler = $this->client->request('GET', '/event/'.$event->getId().'/going');
+        $entityManager->refresh($entityManager->getRepository(User::class)->findOneBy(['email'=>'test-user@test.com']));
+        $entityManager->refresh($entityManager->getRepository(Event::class)->findOneBy(['name'=>'test-event']));
+        $entityManager->flush();
+
+        // Verify user is no longer going and event attending count is 0
+        $this->assertFalse($user->getGoingTo()->contains($event),"user shouldnt be going to event anymore");
+        $this->assertEquals(0, $event->getAttending(),'event shouldnt have any more attending people ');
     }
 
 
